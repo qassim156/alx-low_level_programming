cf0536c476ff323380497d1ae75a49ab80aeea69
qassim156 <iyanuoluwaqassim@gmail.com>
2023-07-20 16:34:38 -0700

    k

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+ my first debugging reafmr file
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include<main.h>
+
+void positive_or_negative.c(int i)
+{
+	if(i > 0)
+		printf("is positive");
+	else if(1 < 0)
+		ptrintf("is negative");
+	else{
+		printf("normak");
+
+	}
+}
