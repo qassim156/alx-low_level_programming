c2de4b3e84e581f599dc19b98c57a72cbc1dfb85
root <root@ADAKO.localdomain>
2024-11-26 12:29:01 +0100

    readme Upload

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include "main.h"
-
+/**@main
+ *myString is an array that stores the text to be printed to the screen
+ *_putchar is the function imporyed to print text
+ *
+ * */
 
 int main(){
 	char myString[8] = "_putchar";
 	int i;
-
+	
 	for(i = 0; i < 8; i++){
 		_putchar(myString[i]);
-
 	}
+	
 	_putchar('\n');
 	return 0;
 }
